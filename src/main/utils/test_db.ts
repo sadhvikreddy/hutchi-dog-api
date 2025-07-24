761760a6065af93bfb553e92dc0a169c6a83373c
sadhvik <sadhvikreddy1997@gmail.com>
2025-07-25 00:00:40 +0100

    cleanup: removing test_db code

--- src/main/utils/test_db.ts
+++
@@ -1,8 +0,0 @@
-import db from "@/main/setup/setup_db"
-
-export default async function test_db() {
-    const snapshot = await db.collection("dogs").get()
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    })
-}
