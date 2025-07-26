b4c7d0dac24a18fb09aabaa3d495a9cb2d1f470d
sadhvik <sadhvikreddy1997@gmail.com>
2025-07-26 02:23:36 +0100

    chore: controller update

--- src/application/data/contracts/controller.ts
+++ src/application/data/contracts/controller.ts
@@ -1,15 +1,11 @@
 import { RequestPayload } from "@/application/data/interfaces/core/RequestPayload";
 
 export default abstract class Controller {
-    abstract run(payload: RequestPayload): Promise<Response>
-    abstract buildValidator(payload: RequestPayload): boolean
+    abstract run(payload: RequestPayload): any;
+    abstract buildValidator(payload: RequestPayload): void
 
     async validateAndRun(payload: RequestPayload) {
-        const validationError = this.buildValidator(payload)
-
-        if(!validationError) {
-            // throw BAD REQUEST (400)
-        }
+        this.buildValidator(payload);
 
         try {
             const response = await this.run(payload);
