06959fa912c2d578f93229f31b1f04db5c934997
sadhvik <sadhvikreddy1997@gmail.com>
2025-07-27 02:20:08 +0100

    feat: delete variant router

--- src/main/setup/setup-routers.ts
+++ src/main/setup/setup-routers.ts
@@ -2,6 +2,7 @@ import { Express } from 'express';
 import { upsertBreedRouter, createManyByJsonRouter, createVariantRouter } from '../routers';
 import bodyParser from 'body-parser';
 import { deleteBreedRouter } from '../routers/delete-breed-router';
+import { deleteVariantRouter } from '../routers/delete-variant-router';
 
 // maybe todo: pure function maybe.
 export const setupRouters = async (app: Express): Promise<void> => {
@@ -9,12 +10,14 @@ export const setupRouters = async (app: Express): Promise<void> => {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     // Routes
-
     // create and upsert
     app.put('/upsert/dog-breed/:name', upsertBreedRouter())
     app.post('/json', createManyByJsonRouter())
-    app.put('/create/dog/:name/variant/:variant', createVariantRouter())
+    app.put('/create/dog-breed/:name/variant/:variant', createVariantRouter())
 
     // delete
     app.delete('/delete/dog-breed/:name', deleteBreedRouter())
+    app.delete('/delete/dog-breed/:name/variant/:variant', deleteVariantRouter())
+
+    // read
 }
