725dd7e672669f94cf226d777e2eeb3c08cb2409
sadhvik <sadhvikreddy1997@gmail.com>
2025-08-01 01:38:43 +0100

    bug fix: create with variant

--- nextjs-webapp/app/actions/dogs-api/create-data.ts
+++ nextjs-webapp/app/actions/dogs-api/create-data.ts
@@ -1,11 +1,17 @@
 "use server"
 
 export default async function createData(name: string, variants?: string[]) {
+    const b = {
+        'variants': variants ?? []
+    }
     const uri = `${process.env.ENDPOINT}/upsert/dog-breed/${name}`
 
     const req = await fetch(uri, {
         method: "PUT",
-        body: JSON.stringify(variants)
+        body: JSON.stringify(b),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
     return req.json();
 }
